191e87c2fc05c237244641760438ef9c0661a2d9
VaderRob <69919131+VaderRob@users.noreply.github.com>
2021-01-31 23:23:41 -0500

    Add files via upload

---
+++ build/precache-manifest.105ab80e9ab27b8c0eee0b821495becf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7497ece8bcab13d349f8b69a1c76519",
+    "url": "/index.html"
+  },
+  {
+    "revision": "289d2c07d513d629f353",
+    "url": "/static/js/2.4bf62a69.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.4bf62a69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "779284895422ed8ac696",
+    "url": "/static/js/main.d6fd9eed.chunk.js"
+  },
+  {
+    "revision": "1316b828eda579fe06fd",
+    "url": "/static/js/runtime-main.86007643.js"
+  }
+]);
